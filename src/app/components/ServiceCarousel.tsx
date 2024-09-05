a2f1e1cd02b129c0276424641ecb5903effb669e
ANNIE CROTEAU <anniecroteauc@gmail.com>
2024-09-05 10:57:26 +0200

    package: daisy UI config

--- src/app/components/ServiceCarousel.tsx
+++ src/app/components/ServiceCarousel.tsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const ServiceCarousel = () => {
-  return <div>ServiceCarousel</div>;
+  return (
+    <div className="carousel carousel-center bg-background max-w-md space-x-4 p-4">
+      <div className="carousel-item relative">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+          className="rounded-md"
+        />
+        <div className="absolute inset-0 bg-background bg-opacity-25"></div>
+        <h1 className="z-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-3xl">
+          Enduits
+        </h1>
+      </div>
+      <div className="carousel-item relative">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+          className="rounded-md"
+        />
+        <div className="absolute inset-0 bg-background bg-opacity-25"></div>
+        <h1 className="z-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-3xl">
+          Enduits
+        </h1>
+      </div>
+      <div className="carousel-item relative">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+          className="rounded-md"
+        />
+        <div className="absolute inset-0 bg-background bg-opacity-25"></div>
+        <h1 className="z-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-3xl">
+          Enduits
+        </h1>
+      </div>
+      <div className="carousel-item relative">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+          className="rounded-md"
+        />
+        <div className="absolute inset-0 bg-background bg-opacity-25"></div>
+        <h1 className="z-10 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-3xl">
+          Enduits
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceCarousel;
