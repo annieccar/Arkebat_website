00a3aa12124e7fef9bccee80afb382c06fe2686f
ANNIE CROTEAU <anniecroteauc@gmail.com>
2025-12-06 13:39:34 +0100

    Add google analytics gtags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import "./globals.css";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-Inter" });
 const josefin_sans = Josefin_Sans({
@@ -45,10 +46,32 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="fr"
       className={`${inter.variable} ${josefin_sans.variable} ${overpass.variable} ${abeezee.variable} ${montserrat.variable}`}
     >
       <body className="min-h-screen flex flex-col items-center bg-background">
+        {/* ------------------------- */}
+        {/*   GOOGLE ANALYTICS GA4   */}
+        {/* ------------------------- */}
+
+        {/* Chargement du script gtag */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9C78XMPDS2"
+        />
+
+        {/* Configuration du tracking */}
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9C78XMPDS2');
+          `}
+        </Script>
+
+        {/* ------------------------- */}
+
         <Navbar />
         <main className="grow w-full flex flex-col items-center mb-10 lg:mb-20">
           {children}
